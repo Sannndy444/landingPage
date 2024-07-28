59b9f8d301ac9409ad186e42285b23e7e35c4791
Sannndy444 <sandyriswantino1933@gmail.com>
2024-07-28 16:29:26 +0700

    mengubah sebagian html dan css, dan menambahkan javascript untuk hamburger menu pada landing page

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    document.querySelector('.nav1').classList.toggle('active');
+    document.querySelector('.nav2').classList.toggle('active');
+});
