4cb6ec0001ec21c60dfdc68627fca6ff8c8b77be
Sannndy444 <sandyriswantino1933@gmail.com>
2024-08-06 09:06:25 +0700

    menambahkan styling extra dan mengubah fitur lokasi dengan menggunakan api gmaps dan js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,23 @@ function toggleSidebar() {
         sidebar.style.width = '250px';
     }
 }
+
+function initialize() {
+    var propertiPeta = {
+        center:new google.maps.LatLng(-7.753525738020103, 113.20667171989645),
+        zoom:9,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+    
+    var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
+    
+    // membuat Marker
+    var marker=new google.maps.Marker({
+        position: new google.maps.LatLng(-7.753525738020103, 113.20667171989645),
+        map: peta
+    });
+
+}
+
+  // event jendela di-load  
+google.maps.event.addDomListener(window, 'load', initialize);
